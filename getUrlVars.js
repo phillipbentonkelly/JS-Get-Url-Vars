54e5f003d572253b7c869b9a4e9b9d4cb4cb1525
Phillip B. Kelly <phillipbentonkelly@gmail.com>
2013-05-01 18:42:12 -0300

    Update getUrlVars.js
    Streamlined the return object. The returned object contains all query params in addition to the param you are looking to match. Additionally, you can check if the param is found by checking .found parameter; It returns a boolean value, default is false.

--- getUrlVars.js
+++ getUrlVars.js
@@ -1,20 +1,35 @@
-var GetUrlVars = {};
+var MapUrlVars = {};
 (function(){
-  GetUrlVars = function(){
-        if( !(this instanceof GetUrlVars))
-            return new GetUrlVars();
+	MapUrlVars = function(){
+        if( !(this instanceof MapUrlVars))
+            return new MapUrlVars();
 
-		this.winLoc = window.location;
+        this.response = { found: false, queryParams: {}, match: {} };
+		this.queryScope = window.location;
 
-		if(this.winLoc.search.length > 0){
-			var urlVarString = this.winLoc.search.replace("?", "");
+		if(this.queryScope.search.length > 0){
+			var urlVarString = this.queryScope.search.replace("?", "");
 			var urlVarStringArr = urlVarString.split("&");
 			for(var i = 0; i < urlVarStringArr.length; i++){
 				var currVarArr = urlVarStringArr[i].split("=");
 				if(currVarArr[0].length > 0 && currVarArr[1]){
 					this[currVarArr[0]] = currVarArr[1];
+
+					this.response.queryParams[currVarArr[0]] = currVarArr[1];
 				}
 			}
 		}
 	};
+
+	MapUrlVars.prototype = {
+		isAvailable: function( queryParam, type ){
+			if(typeof(this.response.queryParams[queryParam]) === type && this.response.queryParams[queryParam].length > 0){
+				this.response.found = true;
+				this.response.match = this.response.queryParams[queryParam];
+			}
+
+			return this.response;
+		}
+	};
+	//MapUrlVars().isAvailable('q', 'string');
 })();
